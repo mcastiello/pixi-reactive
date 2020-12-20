423705ce57f89f29d5723defa78cebe811de2f8b
Marco Castiello <marco.castiello@gmail.com>
2020-12-20 11:33:37 +0100

    Reformatted

--- src/props/PixiCanvasProps.ts
+++ src/props/PixiCanvasProps.ts
@@ -2,11 +2,12 @@ import { LoadResourceType } from '../types';
 import { RenderingEventType } from '../types/GenericEventTypes';
 import { PixiHtmlContainerProps } from './PixiHtmlContainerProps';
 
-export type PixiCanvasProps = RenderingEventType & PixiHtmlContainerProps & {
-  className?: string;
-  width?: number;
-  height?: number;
-  retina?: boolean;
-  speed?: number;
-  textures?: LoadResourceType;
-};
+export type PixiCanvasProps = RenderingEventType &
+  PixiHtmlContainerProps & {
+    className?: string;
+    width?: number;
+    height?: number;
+    retina?: boolean;
+    speed?: number;
+    textures?: LoadResourceType;
+  };

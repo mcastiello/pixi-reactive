c131e35f70ecc5970b0dc3e7ca13d4eb991e1efa
Marco Castiello <marco.castiello@gmail.com>
2021-01-05 23:17:34 +0100

    Removed references to PixiParticles from the todo list

--- docs/src/views/pages/Index.tsx
+++ docs/src/views/pages/Index.tsx
@@ -59,11 +59,6 @@ const Index: React.FC = () => {
         The following are a few things I'm going to add in the next few releases (in order of priority).
         <ul>
           <li>
-            <StyledCode>PixiParticle</StyledCode> - This is probably going to be an external plugin, so that if you don't need it, you won't
-            have it, but it's going to integrate support for{' '}
-            <StyledLink href={'https://github.com/pixijs/pixi-particles'} text={'Particle Effects'} external target={'_blank'} />.
-          </li>
-          <li>
             <StyledCode>PixiSpine</StyledCode> - Another external plugin, this one is going to integrate support for the{' '}
             <StyledLink href={'https://github.com/pixijs/pixi-spine'} text={'Spine Runtime'} external target={'_blank'} />.
           </li>
